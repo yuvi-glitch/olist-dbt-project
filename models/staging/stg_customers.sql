088b8121cbd830d6c420964ba535619333b85e67
uv <yyuvi2508@gmail.com>
2026-05-31 13:33:02 +0530

    Project 1 init - Olist dbt project with first staging model

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,11 @@
+WITH raw_customers AS (
+    SELECT * FROM {{ source('olist', 'olist_customers_dataset') }}
+)
+
+SELECT
+    customer_id,
+    customer_unique_id,
+    customer_zip_code_prefix AS zip_code,
+    customer_city AS city,
+    customer_state AS state
+FROM raw_customers
